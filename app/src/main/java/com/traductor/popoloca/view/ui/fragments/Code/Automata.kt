0ed1b8e98af8f00458be6529d8e5438794403cf3
SanchezJC <sanjuacla@gmail.com>
2021-01-12 18:14:24 -0600

    Automata Versión 0.1

---
+++ app/src/main/java/com/traductor/popoloca/view/ui/fragments/Code/Automata.kt
@@ -0,0 +1,100 @@
+package com.traductor.popoloca.view.ui.fragments.Code
+
+class Automata {
+
+     fun validateWord(word:String):String {
+
+        var validate:String=word
+        var state:Int=0
+        var lexema:String=""
+        var noValidate:String=""
+
+        loop@ for (ind in validate.indices){
+
+            var item:Char = validate[ind]
+            var codeA:Int=item.toInt()
+
+            when(state){
+
+                0 ->
+                    if ((codeA in 65..90)||(codeA in 97..122) ){
+
+                        state=1
+                        lexema+=codeA.toChar()
+
+                        }else if (codeA in 48..57){
+
+                            state=2
+                            lexema+=codeA.toChar()
+
+                        }
+
+                    else{
+
+                        lexema=""
+                        noValidate=word
+                        break@loop
+
+                    }
+
+                1 ->
+                    if((codeA in 65..90 || codeA in 97..122)){
+
+                        state=1
+                        lexema+=codeA.toChar()
+
+                    }
+                        else if(item==' '){
+
+                            state=0
+                            lexema+=codeA.toChar()
+
+                        }
+
+                    else{
+
+                        lexema=""
+                        noValidate=word
+                        break@loop
+
+                    }
+
+                2 ->
+                    if(codeA in 48..57){
+
+                        state=2
+                        lexema+=codeA.toChar()
+
+                    }
+
+                    else{
+
+                        lexema=""
+                        noValidate=word
+                        break@loop
+
+                    }
+
+                else->{
+
+                    lexema=""
+                    noValidate=word
+
+                }
+
+            }
+
+        }
+
+         return if(lexema.isNotEmpty()){
+
+             WordsPopoloca.searchWord(lexema)
+
+         }else{
+
+             noValidate
+
+         }
+
+    }
+}
--- app/src/main/java/com/traductor/popoloca/view/ui/fragments/TraductorFragment.kt
+++ app/src/main/java/com/traductor/popoloca/view/ui/fragments/TraductorFragment.kt
@@ -6,24 +6,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.textfield.TextInputEditText
 
 import com.traductor.popoloca.R
 import com.traductor.popoloca.model.AlphabetLetter
 import com.traductor.popoloca.view.adapter.ButtonAdapter
+import com.traductor.popoloca.view.ui.fragments.Code.Automata
 import com.traductor.popoloca.viewmodel.AlphabetLetterViewModel
 import kotlinx.android.synthetic.main.fragment_abecedario.*
 import kotlinx.android.synthetic.main.fragment_imagen.*
+import kotlinx.android.synthetic.main.fragment_traductor.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class TraductorFragment : Fragment() {
 
+    private lateinit var word:TextInputEditText
+    private lateinit var result: TextView
+    private lateinit var search: Button
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_traductor, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_traductor, container, false)
+        word=view.findViewById(R.id.txtEspanol)
+        result=view.findViewById(R.id.textResult)
+        search=view.findViewById(R.id.btnTraductor)
+
+        search.setOnClickListener {
+
+            var automata=Automata()
+            result.text=automata.validateWord(word.text.toString())
+
+        }
+        return view
     }
 
 }
